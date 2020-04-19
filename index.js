ea1be64439b401817a5a091d4cbecbdf27cf2b31
akhilmhdh <akhilmhdh@gmail.com>
2020-04-20 00:25:22 +0530

    =first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run(){
+    try{
+        const myToken = core.getInput('myToken');
+
+        if (!myToken){
+            throw "Token not found"
+        }
+        const octokit = new github.GitHub('GITHUB_TOKEN');
+        const nwo = process.env['GITHUB_REPOSITORY'] || '/'
+        const [owner, repo] = nwo.split('/')
+        
+        const readme= await octokit.request(`GET /repos/${owner}/${repo}/readme`,{
+            headers: {
+                authorization: `token ${myToken}`,
+                Accept: "application/vnd.github.VERSION.raw"
+              },
+        })
+        console.log(`readme: ${readme}`);
+    }
+    catch(error){
+        core.setFailed(error.message)
+    }
+}
+
+run();
