33c330c173a55c037184c5f0dcf4323183a391f0
akhilmhdh <akhilmhdh@gmail.com>
2020-04-21 16:33:29 +0530

    =capital case v2

--- capitalize.js
+++ capitalize.js
@@ -1,11 +1,8 @@
+function capitalCaseUtil(str){
+    return(str.charAt(0).toUpperCase()+str.substring(1))
+}
+
 exports.toCapitalCase = function (str) {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
-    }
-    // Directly return the joined string
-    return splitStr.join(' '); 
+    return (str.split(" ").map(capitalCaseUtil).join(" "))
   }
 
