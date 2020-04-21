b0311de2f074aa8e852d405b4e894522435c56f3
akhilmhdh <akhilmhdh@gmail.com>
2020-04-21 16:03:02 +0530

    =camal case bug fix v1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const {CamelCaseString} = require('./camelcase')
+const CamelCaseString = require('./camelcase')
 
 async function run(){
     try{
