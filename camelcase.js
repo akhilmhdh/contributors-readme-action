cd909e7b26535ad4c6d9aa5759f22ed5456a03a2
akhilmhdh <akhilmhdh@gmail.com>
2020-04-21 16:06:18 +0530

    =camal case bug fix v2

--- camelcase.js
+++ camelcase.js
@@ -1,4 +1,4 @@
-export default function CamelCaseString(str) {
+exports.CamelCaseString = function (str) {
     return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
       if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
       return index === 0 ? match.toLowerCase() : match.toUpperCase();
